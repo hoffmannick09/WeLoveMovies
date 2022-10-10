83797607815039d9f4c00498396a831c408014f6
Nick Hoffman <101670988+hoffmannick09@users.noreply.github.com>
2022-10-10 11:25:55 -0600

    added migrations

--- src/db/migrations/20221005160145_createReviewsTable.js
+++ src/db/migrations/20221005160145_createReviewsTable.js
@@ -1,8 +1,27 @@
+exports.up = function (knex) {
+    return knex.schema.createTable("reviews", (table) => {
+     table.increments("review_id").primary()
+     table.text("content")
+     table.integer("score")
 
-exports.up = function(knex) {
+      table.integer("critic_id").unsigned().notNullable();
+      table 
+        .foreign("critic_id")
+        .references("critic_id")
+        .inTable("critics")
+        .onDelete("cascade");
   
-};
-
-exports.down = function(knex) {
+      table.integer("movie_id").unsigned().notNullable();
+      table
+        .foreign("movie_id")
+        .references("movie_id")
+        .inTable("movies")
+        .onDelete("cascade");
   
-};
+      table.boolean("is_showing");
+    });
+  };
+  
+  exports.down = function (knex) {
+    return knex.schema.dropTable("reviews");
+  };
